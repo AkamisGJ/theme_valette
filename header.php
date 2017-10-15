f7b6f9bb21e249c4b90d606bfa6bad8845b57713
Akamis <mrtetex@gmail.com>
2017-10-15 18:28:17 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+ <title><?php bloginfo('name'); ?></title>
+ <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+ <img id="banner" alt="banner" src="wp-content/themes/valette/img/banner.jpg">
+</div>
+
+<?php //wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
