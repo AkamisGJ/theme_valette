4d59a7e505a98fe217c6fe60327022d5904b15c1
Akamis <mrtetex@gmail.com>
2017-10-19 21:35:56 +0200

    add font and wp menu
    Update

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <head>
  <title><?php bloginfo('name'); ?></title>
  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+ <link rel="stylesheet" type="text/css" href="./font/stylesheet.css">
 </head>
 <body>
 <div id="wrapper">
@@ -9,4 +10,6 @@
  <img id="banner" alt="banner" src="wp-content/themes/valette/img/banner.jpg">
 </div>
 
-<?php //wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'Main_menu',
+							'menu_id' => 'main_menu',
+							'container_id' => 'test', ) ); ?>
